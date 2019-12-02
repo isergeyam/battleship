91f2cdea403d9a09f7f73bef586828d26725e7c5
Nikita Fedorets <fns1999@yandex.ru>
2019-12-02 14:32:17 +0300

    Imports cahnged

--- battleship-client/src/MainPage/TopPlayers.jsx
+++ battleship-client/src/MainPage/TopPlayers.jsx
@@ -3,29 +3,29 @@ import './main_page.css'
 import { userService } from '../_services';
 
 
-// class PlayersList extends React.Component {
-//     render() {
-//         const zip = (arr1, arr2) => arr1.map((k, i) => [k, arr2[i]]);
-//         const user_winrate = zip(this.props.users, this.props.winrate);
-//         const rows = user_winrate.map(user =>
-//             <tr>
-//                 <td>{user[0]}</td>
-//                 <td>{user[1]}</td>
-//             </tr>
-//         );
-//         return (
-//             <table>
-//                 <tbody>
-//                     <tr>
-//                         <th>Username</th>
-//                         <th>Win rate</th>
-//                     </tr>
-//                     {rows}
-//                 </tbody>
-//             </table>
-//         )
-//     }
-// }
+class PlayersList extends React.Component {
+    render() {
+        const zip = (arr1, arr2) => arr1.map((k, i) => [k, arr2[i]]);
+        const user_winrate = zip(this.props.users, this.props.winrate);
+        const rows = user_winrate.map(user =>
+            <tr>
+                <td>{user[0]}</td>
+                <td>{user[1]}</td>
+            </tr>
+        );
+        return (
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Username</th>
+                        <th>Win rate</th>
+                    </tr>
+                    {rows}
+                </tbody>
+            </table>
+        )
+    }
+}
 
 class TopPlayers extends React.Component {
     constructor(props) {
@@ -39,16 +39,16 @@ class TopPlayers extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         const response = this.props.get_top_players();
-        this.setState({users: response[0], winrate: response[1]});
+        this.setState({ users: response[0], winrate: response[1] });
 
     }
 
     render() {
         return (
             <div>
-                {/* <form name="form" onSubmit={this.handleSubmit}> */}
-                {/* <PlayersList users={this.state.users} winrate={this.state.winrate} /> */}
-            {/* </form> */}
+                <form name="form" onSubmit={this.handleSubmit}>
+                    <PlayersList users={this.state.users} winrate={this.state.winrate} />
+                </form>
             </div>
 
         );
