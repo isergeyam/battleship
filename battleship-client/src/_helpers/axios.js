e0a12eda8a3f2b5ed995d63c109f42387973cde4
Sergey Grigoryants <isergey.am@gmail.com>
2019-12-09 23:53:48 +0300

    Added docker support

--- battleship-client/src/_helpers/axios.js
+++ battleship-client/src/_helpers/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import config from 'config';
 
+const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:9090/api';
+
 export default axios.create({
-  baseURL: config.apiUrl
+  baseURL: REACT_APP_API_BASE_URL
 });
