926a9ae17a028c0bc275978a8c306f0318cb7096
Sergey Grigoryants <isergey.am@gmail.com>
2019-12-10 21:34:11 +0300

    fixed base url

--- battleship-client/src/_helpers/axios.js
+++ battleship-client/src/_helpers/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import config from 'config';
-
-const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:9090/api';
+const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://3.136.160.86:9090/api';
 
 export default axios.create({
   baseURL: REACT_APP_API_BASE_URL
