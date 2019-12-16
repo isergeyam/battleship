22167c61a6f40f611dd56513b709be483fada576
Sergey Grigoryants <isergey.am@gmail.com>
2019-12-16 17:11:51 +0300

    Fixed webpack config

--- battleship-client/webpack.config.js
+++ battleship-client/webpack.config.js
@@ -1,36 +1,15 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader'
-            },
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })],
-    devServer: {
-        historyApiFallback: true,
-        host: '0.0.0.0'
-    },
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:9090/api'
-        })
-    },
-    // resolve: {
-    //     extensions: ['', '.js', '.jsx', '.css'],
-    //     modulesDirectories: [
-    //         'node_modules'
-    //     ]
-    // }
+  mode: 'development',
+  resolve: { extensions: ['.js', '.jsx'] },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  devServer: { historyApiFallback: true, host: '0.0.0.0' },
+  externals: { config: JSON.stringify({ apiUrl: 'http://localhost:9090/api' }) }
 }
