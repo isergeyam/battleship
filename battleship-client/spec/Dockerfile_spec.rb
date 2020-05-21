c7357c6f8fb61ea3c5f00e27970a2cc0ff31e45c
Sergey Grigoryants <isergey.am@gmail.com>
2020-05-21 18:52:17 +0300

    Added rspec for client.

---
+++ battleship-client/spec/Dockerfile_spec.rb
@@ -0,0 +1,42 @@
+# spec/Dockerfile_spec.rb
+
+require "serverspec"
+require "docker"
+require_relative "spec_helper"
+require "rspec/wait"
+
+describe "image specs" do
+  before(:all) do
+    image = Docker::Image.build_from_dir('.')
+
+    set :os, family: :alpine
+    set :backend, :docker
+    set :docker_image, image.id
+  end
+
+	it "nginx installed" do
+    expect(package("nginx")).to be_installed
+  end
+
+end
+
+describe "container specs" do
+	before(:all) do
+		image = Docker::Image.build_from_dir('.')
+		@container = image.run()
+
+		set :os, family: :alpine
+		set :backend, :Docker
+		set :docker_container, @container.id
+	end
+
+	describe port(80), wait: { timeout: 5 } do
+		RSpec::Wait::wait_for { should be_listening }
+		it { should be_listening }
+	end
+
+	after(:all) do
+		@container.kill
+		@container.delete(:force => true)
+	end
+end
